7fe84d8663801747d8b71686be0023acc4f81aa8
msarkor <ms866@njit.edu>
2020-07-01 17:40:26 -0400

    Feature: Added Subtraction functionality

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,27 @@
+def Addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
 
 class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
         pass
+
+    def Addition(self, a, b):
+        self.result = Addition(a, b)
+        return self.result
+
+    def subtraction(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+
+
 
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,14 +1,26 @@
 import unittest
 from Calculator import Calculator
+import csv
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_calulator_return_difference(self):
+        self.calculator.subtraction(1, 4)
+        self.assertEqual(self.calculator.result,-3)
 
     def test_results_property_calculator(self):
-            calculator = Calculator()
-            self.assertEqual(calculator.result,4)
+        calculator = Calculator()
+        self.assertEqual(calculator.result,0)
+
+    def test_add_method_calculator(self):
+        self.calculator.Addition(2, 2)
+        self.assertEqual(self.calculator.result,4)
+
 
 if __name__ == '__main__':
     unittest.main()
